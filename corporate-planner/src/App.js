0727625057565478a7d98846e6c7135b61e094e1
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-19 09:28:02 -0500

    add form built

--- corporate-planner/src/App.js
+++ corporate-planner/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Login from "./components/Login";
 import Registration from "./components/Registration";
 import { Route, Link } from "react-router-dom";
+import AddEvent from "./components/AddEvent";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Link to="/Login">Login</Link>
       <Route path="/Login" component={Login} />
       <Route path="/Registration" component={Registration} />
+      <Route path="/addevent" component={AddEvent} />
     </div>
   );
 }
