502e3f8e327aa74f4c3347e547747d9325110821
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-18 23:11:27 -0500

    test div

--- corporate-planner/src/App.js
+++ corporate-planner/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
-import Login from './components/Login';
-import Registration from './components/Registration';
-import { Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Login from "./components/Login";
+import Registration from "./components/Registration";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <h1>test</h1>
       <Route path="/Login" component={Login} />
       <Route path="/Registration" component={Registration} />
     </div>
